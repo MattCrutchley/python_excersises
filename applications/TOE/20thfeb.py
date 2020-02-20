722d184de7712d016347d096efa9c356cafdb199
MattCrutchley <mcrutchley8@gmail.com>
2020-02-20 17:01:03 +0000

    added toe

---
+++ applications/TOE/20thfeb.py
@@ -0,0 +1,8 @@
+def bert(str):
+    list_ = str.lower().split("bert")
+    if str.lower().count("bert") < 2:
+        return ""
+    else:
+        return list_[-2]
+
+print(bert("xxBertfridgebERtyy"))
